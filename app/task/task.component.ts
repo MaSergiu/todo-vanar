d77eb0bf221fb72b48257988b299083d714a9e4c
MaSergiu <masergiu@gmail.com>
2016-09-28 13:00:47 +0300

    Task with button

--- app/task/task.component.ts
+++ app/task/task.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
 })
 export class TaskComponent {
     title = "Title of my task!";
-    status = 0; // 0 => just created; 1 => started; 2 => finished; 3 => canceled; 4 => deleted
+    // 0 => just created; 1 => started; 2 => finished; 3 => canceled; 4 => deleted
+    status = 0;
+
+    deleteTask () {
+        this.status = 4;
+    }
+
+    /*
+        HOME WORK
+        if just created - show start and delete buttons
+        if started - show finish and cancel buttons
+        if finished - show delete button
+        if canceled - show start and delete buttons
+        if deleted - do not show any buttons
+    */
 }
