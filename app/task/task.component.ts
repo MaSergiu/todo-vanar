e43052895d32aa9f8626b391cee8bc172e484f2f
MaSergiu <masergiu@gmail.com>
2016-09-30 06:31:14 +0300

    Added buttons

--- app/task/task.component.ts
+++ app/task/task.component.ts
@@ -9,16 +9,19 @@ export class TaskComponent {
     // 0 => just created; 1 => started; 2 => finished; 3 => canceled; 4 => deleted
     status = 0;
 
+    startWork () {
+        this.status = 1;
+    }
+
+    finishIt () {
+        this.status = 2;
+    }
+
+    cancelIt () {
+        this.status = 3;
+    }
+
     deleteTask () {
         this.status = 4;
     }
-
-    /*
-        HOME WORK
-        if just created - show start and delete buttons
-        if started - show finish and cancel buttons
-        if finished - show delete button
-        if canceled - show start and delete buttons
-        if deleted - do not show any buttons
-    */
 }
